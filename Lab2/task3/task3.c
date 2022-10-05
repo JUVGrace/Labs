311d928480ace372e737566799d1aaa5b40b66e3
JUVGrace <danshenley@mail.ru>
2022-10-05 21:07:08 +0300

    fix lab2 task3

--- Lab2/task3/task3.c
+++ Lab2/task3/task3.c
@@ -1,56 +1,40 @@
-#include<stdio.h>
+#include<stdio.h> 
 
 int main()
+
 {
-	int P, Q;
-	printf("Input P and Q\n");
-	if (scanf_s("%d %d", &P, &Q) == 2)
+	int P, Q, div, i;
+	printf("Input P and Q:\n");
+	if (scanf_s("%d%d", &P, &Q) == 2)
 	{
-		
-
-		if (P != 1 && Q != 1)
+		if (P == 0 || Q == 0)
 		{
-			if (P % Q == 0 || Q % P == 0)
-			{
-				printf("%d and %d - not coprime numbers\n", P, Q);
-				return 0;
-			}
-			if (P == 0 && Q != 0 || P != 0 && Q == 0)
-			{
-				printf("%d and %d - not coprime numbers\n", P, Q);
-				return 0;
-			}
-			if (P % Q != 0 || Q % P != 0)
-			{
-				printf("%d and %d - Coprime numbers\n", P, Q);
-				return 0;
-			}
-			
+			printf("Wrong input");
+			return 0;
 		}
 
-
-		if (P == 1 || Q == 1)
+		for (i = 1; i <= P; i++)
 		{
-			if (P == 1 && Q == 1)
+			if (P % i == 0 && Q % i == 0)
 			{
-				printf("%d and %d - Coprime numbers\n", P, Q);
-				return 0;
+				div = i;
 			}
-			if (P == 1 && Q != 0 || P != 0 && Q == 1)
-			{
-				printf("%d and %d - Coprime numbers\n", P, Q);
-				return 0;
-			}
-			if (P == 1 && Q == 0 || P == 0 && Q == 1)
-			{
-				printf("%d and %d - not coprime numbers\n", P, Q);
-				return 0;
-			}
+
 		}
-		
+		if (div == 1)
+		{
+			printf("%d and %d are CO-PRIME NUMBERS.", P, Q);
+		}
+		else 
+		{ 
+			printf("%d and %d are NOT CO-PRIME NUMBERS.", P, Q); 
+		}     
+		return 0;
 	}
 	else
-		printf("Wrong Input\n");
-	
-	return 0;
+	{
+		printf("Wrong input");
+		return 0;
+
+	}
 }
